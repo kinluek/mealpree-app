c9b4c223dd32dc0456b1dc321ed2010b5bef39e7
Si Chan <si.chan@bjss.com>
2021-02-04 22:38:40 +0000

    add material ui

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,41 +1,72 @@
-import { Link } from 'react-router-dom';
-import { Flex, Text } from 'rebass';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router';
 import { useUserContext } from '../../context/user.context';
 import { auth } from '../../firebase/auth';
 
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  logo: {
+    display: 'inline-block',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+}));
+
 const Header: React.FunctionComponent = () => {
+  const classes = useStyles();
+  const history = useHistory();
   const { user } = useUserContext();
 
   const signOut = () => {
-    auth.signOut().catch(console.log);
+    return auth
+      .signOut()
+      .then(() => history.push('/'))
+      .catch(console.log);
   };
 
   return (
-    <Flex color="white" bg="#66CDAA" justifyContent="space-between">
-      <Link to="/">
-        <Text p={2} fontWeight="bold" color="white">
-          mealpree
-        </Text>
-      </Link>
-      {user ? (
-        <Flex>
-          <Text onClick={signOut}>Sign Out</Text>
-        </Flex>
-      ) : (
-        <Flex>
-          <Link to="/signup">
-            <Text p={2} fontWeight="bold" color="white">
-              Sign Up
-            </Text>
-          </Link>
-          <Link to="/signin">
-            <Text p={2} fontWeight="bold" color="white">
-              Sign In
-            </Text>
-          </Link>
-        </Flex>
-      )}
-    </Flex>
+    <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        <div className={classes.toolbarTitle}>
+          <Typography onClick={() => history.push('/')} variant="h6" color="inherit" className={classes.logo}>
+            mealpree
+          </Typography>
+        </div>
+        {!user ? (
+          <Button onClick={() => history.push('/signin')} color="primary" variant="outlined" className={classes.link}>
+            Login
+          </Button>
+        ) : (
+          <Button onClick={signOut} color="primary" variant="outlined" className={classes.link}>
+            Logout
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- src/pages/sign-in/sign-in.page.tsx
+++ src/pages/sign-in/sign-in.page.tsx
@@ -1,15 +1,44 @@
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import { useState } from 'react';
-import { Box, Flex, Button } from 'rebass';
-import { Label, Input } from '@rebass/forms';
-import { signInWithEmailAndPassword, signInWithGoogle } from '../../firebase/auth';
 import { useHistory } from 'react-router';
+import { signInWithEmailAndPassword, signInWithGoogle } from '../../firebase/auth';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const SignInPage: React.FunctionComponent = () => {
+  const classes = useStyles();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLDivElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
       const { userCredential } = await signInWithEmailAndPassword(email, password);
@@ -35,42 +64,61 @@ const SignInPage: React.FunctionComponent = () => {
   };
 
   return (
-    <Flex justifyContent="center">
-      <Flex flexDirection="column">
-        <Box as="form" onSubmit={handleSubmit} py={3} sx={{ justifyContent: 'center' }}>
-          <Flex mx={-2} mb={3} flexDirection="column">
-            <Box px={2} my={1}>
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Box>
-            <Box px={2} my={1}>
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Box>
-          </Flex>
-          <Button bg="#66CDAA" width="100%">
-            Sign in
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+          <Button type="submit" fullWidth variant="contained" color="default" className={classes.submit}>
+            Sign In
           </Button>
-        </Box>
-        <Button color="white" bg="blue" width="100%" onClick={handleGoogleSignIn}>
-          Sign in with Google
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link onClick={() => history.push('/signup')} variant="body2">
+                Don't have an account? Sign Up
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+        <Typography component="p">- OR -</Typography>
+        <Button onClick={handleGoogleSignIn} fullWidth variant="contained" color="primary" className={classes.submit}>
+          Sign With Google
         </Button>
-      </Flex>
-    </Flex>
+      </div>
+    </Container>
   );
 };
 
