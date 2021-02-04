c306de1e1e650b78fe917e5e4107356d6e61fe6e
Si Chan <si.chan@bjss.com>
2021-02-04 22:38:40 +0000

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import SignInPage from './pages/sign-in/sign-in.page';
 import SignUpPage from './pages/sign-up/sign-up.page';
+import HomePage from './pages/home-page/homepage.page';
 import Header from './components/header/header.component';
 import UserProvider from './context/user.context';
 import { Route, Switch } from 'react-router';
-
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <UserProvider>
-        <Header />
-        <Switch>
-          <Route exact path="/" render={() => <h1>Welcome to Mealpree</h1>} />
-          <Route exact path="/signup" component={SignUpPage} />
-          <Route exact path="/signin" component={SignInPage} />
-        </Switch>
-      </UserProvider>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <div className="App">
+        <UserProvider>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/signup" component={SignUpPage} />
+            <Route exact path="/signin" component={SignInPage} />
+          </Switch>
+        </UserProvider>
+      </div>
+    </React.Fragment>
   );
 };
 
